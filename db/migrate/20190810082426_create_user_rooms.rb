b10cccabec0d52b392196b0315beb0f4f353f1f3
panage-jp <joojoojoo6217@gmail.com>
2019-08-29 15:45:07 +0900

    update

--- db/migrate/20190810082426_create_user_rooms.rb
+++ db/migrate/20190810082426_create_user_rooms.rb
@@ -1,6 +1,6 @@
 class CreateUserRooms < ActiveRecord::Migration[5.0]
   def change
-    create_table :user_rooms do |t|
+    create_table :users_rooms do |t|
       t.integer :user_id, null: false, foreign_key: true
       t.integer :room_id, null: false, foreign_key: true
       t.timestamps
